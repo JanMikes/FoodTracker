0e63344ab7aa89c9287156c11b2ba69ad0546bf8
Jan Mikeš <j.mikes@me.com>
2020-02-15 20:37:05 +0100

    Initial Commit

---
+++ FoodTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTracker
+//
+//  Created by Jan Mikeš on 15/02/2020.
+//  Copyright © 2020 Jan Mikeš. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
